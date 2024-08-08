7adf2f118602c8c5eaccd672213e23841d205f4b
Manases Lovera <manaseslovera07@gmail.com>
2024-08-07 23:30:32 -0400

    new changes

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,17 +1,21 @@
 import './Card.css';
 
-type Props = {}
+type Props = {
+    companyName: string;
+    ticker: string;
+    price: number;
+}
 
-const Card = (props: Props) => {
+const Card = ({companyName, ticker, price}: Props) => {
     return (
         <div className="card">
             <img 
                 src=""
-                alt="Image"
+                alt="Card"
             />
             <div className="details">
-                <h2>AAPL</h2>
-                <p>$110</p>
+                <h2>{companyName} ({ticker})</h2>
+                <p>${price}</p>
             </div>
             <p className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, corrupti.</p>
         </div>
